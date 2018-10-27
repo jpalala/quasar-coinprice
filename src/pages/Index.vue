e1330525143e241452c88486953ac7e63cf6703a
Joe Palala <jose.palala@live.com>
2018-10-28 00:33:28 +0800

    make input react on change of message
    i can now lookup the price based on currency

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,8 @@
 <template>
   <q-page class="flex flex-center">
+  <!-- Single Line Input -->
+  <q-input stack-label="Currency" v-model="message" placeholder="Gigi" @blur="getPriceForCode" />
+    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
     {{ info }}
   </q-page>
 </template>
@@ -12,15 +15,27 @@ import axios from 'axios';
 
 export default {
   name: 'PageIndex',
-  data() {
-      return {
-        info: null
-      }
+  data () {
+    return {
+      info: null,
+      message: 'quasar is crazy!'
+
+    }
   },
   mounted () {
     axios
       .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-      .then(response => (this.info = response));
+      .then(response => (this.info = response.data.bpi));
+  },
+  methods: {
+    getPriceForCode () {
+      if (this.message) {
+        console.log('Check the price for bitcoin under currency', this.message);
+        if (this.message === 'gbp') {
+          this.info = this.info.GBP.rate;
+        }
+      }
+    }
   }
-};
+}
 </script>
