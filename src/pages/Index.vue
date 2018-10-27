4d3dc76f60ceb22af8e33b180efe8f4cdfbf2ed8
Joe Palala <jose.palala@live.com>
2018-10-27 12:47:23 +0800

    initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-page class="flex flex-center">
+    {{ info }}
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'PageIndex',
+  data() {
+      return {
+        info: null
+      }
+  },
+  mounted () {
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => (this.info = response));
+  }
+};
+</script>
